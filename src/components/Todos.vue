a70db3eaae16a26e399c18252fe06da20287c92d
Damoon <d.shahhosseini@yahoo.com>
2020-03-16 22:39:36 -0400

    Initial commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"/>
+        </div>
+    </div>
+</template>
+
+<script>
+    import TodoItem from './TodoItem';
+
+    export default {
+        name: "Todos",
+        props: ["todos"],
+        components: {
+            TodoItem
+        }
+    }
+</script>
+
+<style scoped>
+    .todo-item {
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+  }
+  .is-complete {
+    text-decoration: line-through;
+  }
+  .del {
+    background: #ff0000;
+    color: #fff;
+    border: none;
+    padding: 5px 9px;
+    border-radius: 50%;
+    cursor: pointer;
+    float: right;
+  }
+</style>
